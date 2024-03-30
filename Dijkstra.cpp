dd4199e08c7109263f1be8c00d6a529f5ee4b976
DESKTOP-QE9UGV8\redbindy <redbindy@naver.com>
2024-03-30 20:22:32 +0900

    Changed node_t to std::pair and node array to std::pair<index, weight>,  Added constant INF, renamed some variables

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -3,72 +3,92 @@
 #include <queue>
 #include <vector>
 
-typedef struct
-{
-    size_t id;
-    std::list<std::pair<size_t, size_t>> neighbors;
-} node_t;
-
 struct compare
 {
-    bool operator()(std::pair<node_t, size_t>& p1, std::pair<node_t, size_t>& p2)
+    bool operator()(const std::pair<size_t, size_t>& p1, const std::pair<size_t, size_t>& p2)
     {
         return p1.second > p2.second;
     }
 };
 
+enum
+{
+    INF = 99
+};
+
 int main()
 {
+    /*
+    * 입력 형식
+    * 
+    * 첫 입력: 노드의 개수
+    * 이후 입력: 인접행렬
+    * 
+    * 입력에서 99는 무한대 가중치(INF)로 가정하고 작성
+    * 
+    * 입력 예시
+    * 6
+    * 0 2 1 99 99 99
+    * 2 0 3 99 1 99
+    * 1 3 0 3 1 99
+    * 99 99 3 0 1 4
+    * 99 1 1 1 0 2
+    * 99 99 99  4 2 99
+    * 
+    * 결과는 (0번 노드)->(다른 노드) 최단비용
+    * 
+    * 입력 예시에 대한 결과
+    * 0 2 1 3 2 4
+    */
+
     size_t nodeCount;
     std::cin >> nodeCount;
 
-    std::vector<node_t> nodes;
-    nodes.reserve(nodeCount);
+    // std::pair<노드 번호, 가중치> 리스트의 배열
+    std::vector<std::list<std::pair<size_t, size_t>>> adjacencyLists;
+    adjacencyLists.reserve(nodeCount);
 
+    // from -> to 방향
     for (size_t fromIndex = 0; fromIndex < nodeCount; ++fromIndex)
     {
-        node_t node;
-        node.id = fromIndex;
+        adjacencyLists.push_back(std::list<std::pair<size_t, size_t>>());
 
         size_t distance = 0;
 
-        size_t toIndex;
-        for (toIndex = 0; toIndex < nodeCount; ++toIndex)
+        for (size_t toIndex = 0; toIndex < nodeCount; ++toIndex)
         {
             std::cin >> distance;
 
-            if (toIndex == fromIndex || distance == 99)
+            if (toIndex == fromIndex || distance == INF)
             {
                 continue;
             }
 
-            node.neighbors.push_front({ toIndex, distance });
+            adjacencyLists[fromIndex].push_front({ toIndex, distance });
         }
-
-        nodes.push_back(node);
     }
 
     std::vector<size_t> minDists;
     minDists.assign(nodeCount, (size_t)-1);
     minDists[0] = 0;
 
-    std::priority_queue<std::pair<node_t, size_t>, std::vector<std::pair<node_t, size_t>>, compare> open;
-    open.push({ nodes[0], 0 });
+    std::priority_queue<std::pair<size_t, size_t>, std::vector<std::pair<size_t, size_t>>, compare> open;
+    open.push({ 0, 0 });
 
     while (!open.empty())
     {
-        node_t node = open.top().first;
-        size_t totalDistance = open.top().second;
+        size_t nodeIndex = open.top().first;
+        size_t currDist = open.top().second;
         open.pop();
 
-        if (minDists[node.id] < totalDistance)
+        if (minDists[nodeIndex] < currDist)
         {
             continue;
         }
 
-        for (std::pair<size_t, size_t>& pair : node.neighbors)
+        for (std::pair<size_t, size_t>& pair : adjacencyLists[nodeIndex])
         {
-            size_t newDist = totalDistance + pair.second;
+            size_t newDist = currDist + pair.second;
 
             if (newDist >= minDists[pair.first])
             {
@@ -76,7 +96,7 @@ int main()
             }
 
             minDists[pair.first] = newDist;
-            open.push({ nodes[pair.first], newDist });
+            open.push({ pair.first, newDist });
         }
     }
 
